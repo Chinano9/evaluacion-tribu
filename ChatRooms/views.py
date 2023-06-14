62bba2207439794208e451681662852541da5cb3
Fernando Ponce Solis <ponceconc@outlook.com>
2023-06-14 03:56:38 -0600

    Urls and views updated
    Login view
    Register view

--- ChatRooms/views.py
+++ ChatRooms/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
-def login(request):
-    return render(request, "login.html")
+@csrf_protect
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = "Nombre de usuario o contraseña incorrectos"
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 
-def register(request):
-    return render(request, "register.html")
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')  # Reemplaza 'login' con el nombre de tu URL de inicio de sesión
+    else:
+        return render(request, 'register.html')
 
 def chat_list(request):
     return render(request, "chat_list.html")
 
 def chat(request):
     return render(request, "chat.html")
-
