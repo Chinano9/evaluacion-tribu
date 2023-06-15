0bc24932e1ebdc4e9d483cfac9ebb7d778e3f4b9
Fernando Ponce Solis <ponceconc@outlook.com>
2023-06-14 19:26:49 -0600

    Created the register, create room and room list urls

--- ChatRooms/urls.py
+++ ChatRooms/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register')
+    path('register/', views.register_view, name='register'),
+    path('rooms/', views.room_list, name='room_list'),
+    path('create_room', views.create_room, name='create_room')
 ]
