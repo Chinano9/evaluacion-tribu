2a7772cf2af94c4bf26c83019c2f2dfcb87ec208
Fernando Ponce Solis <ponceconc@outlook.com>
2023-06-15 00:50:00 -0600

    Models added room field and migrations

--- ChatRooms/models.py
+++ ChatRooms/models.py
@@ -10,3 +10,4 @@ class Message (models.Model):
     message = models.CharField(max_length=370)
     datetime = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, default=1)
